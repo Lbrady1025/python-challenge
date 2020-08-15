8b25967234159758c58c6dfdb0de84bc354f9bdb
Leah Brady <brady.leah@gmail.com>
2020-08-14 20:01:18 -0400

    add months to greatest max/min change

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,14 +17,26 @@ with open(csvpath) as csvfile:
     total_months = len(monthcount)
     pnl_total = sum(pnl_list)
     
+
     pnl_change = []
     for i in range(len(pnl_list)):
         change = int(pnl_list[i] - pnl_list[i-1])
         pnl_change.append(change)
-     
+    
+    #print(f"Change {change}")
+
+    change_total = 0
+    for i in range(len(pnl_change)):
+        change_total =+ int(pnl_change[i])
+    #print(change_total)
     greatest_increase = max(pnl_change)
     greatest_decrease = min(pnl_change)
     
+    max_month_index = pnl_change.index(max(pnl_change))
+    max_month = monthcount[max_month_index]
+
+    min_month_index = pnl_change.index(min(pnl_change))
+    min_month = monthcount[min_month_index]
 
     #pnl_average = change_total
 
@@ -32,7 +44,7 @@ with open(csvpath) as csvfile:
     print("---------------------")
     print(f"Total Months: {total_months}")
     print(f"Total Profits/Losses: ${pnl_total}")
-    #print(f"Average Change: ${pnl_average}")
-    print(f"Greatest Increase in Profits: ${greatest_increase}")
-    print(f"Greatest Decrease in Profits: ${greatest_decrease}")
+    # print(f"Average Change: ${pnl_average}")
+    print(f"Greatest Increase in Profits: {max_month} (${greatest_increase})")
+    print(f"Greatest Decrease in Profits: {min_month} (${greatest_decrease})")
 
