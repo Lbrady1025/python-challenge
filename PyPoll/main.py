ce6310e5408c4f69b67525a513de8e7668620109
Leah Brady <brady.leah@gmail.com>
2020-08-14 22:59:46 -0400

    calculate individual vote counts for each candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,18 +9,40 @@ with open(csvpath) as csvfile:
 
     votes = []
     candidates = []
+    unique_candidates = []
+    candidate1_votes = []
+    candidate2_votes = []
+    candidate3_votes = []
+    candidate4_votes = []
     for row in csvreader:
         votes.append(row[0])
-        candidates.append(row[2])
-    total_votes = len(votes)
-
-    unique_candidates = []
-    for name in candidates:
-        if name not in unique_candidates:
-            unique_candidates.append(name)
+        #candidates.append(row[2])
+        if row[2] not in unique_candidates:
+            unique_candidates.append(row[2])
+        if row[2] == unique_candidates[0]:
+            candidate1_votes.append(row[2])
+        elif row[2] == unique_candidates[1]:
+            candidate2_votes.append(row[2])
+        elif row[2] == unique_candidates[2]:
+            candidate3_votes.append(row[2])
+        elif row[2] == unique_candidates[3]:
+            candidate4_votes.append(row[2])
     
-    candidate1 = unique_candidates[0]
-    candidate2 = unique_candidates[1]
-    candidate3 = unique_candidates[2]
-    candidate4 = unique_candidates[3]
+    total_votes = len(votes)
+    print(len(candidate1_votes))
+    print(len(candidate2_votes))
+
+    #for name in candidates:
+        #if name not in unique_candidates:
+            #unique_candidates.append(name)
+    
+    #candidate1 = unique_candidates[0]
+    #candidate2 = unique_candidates[1]
+    ##candidate3 = unique_candidates[2]
+    #candidate4 = unique_candidates[3]
+
+    #for candidate in candidates:
+        
+    
+    #print(candidate1_votes)
     
